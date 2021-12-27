d158fe5c56171ec315f735dd6312cc767c14b1e7
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-27 10:49:03 -0300

    Entradas - setas do teclado

--- js/script.js
+++ js/script.js
@@ -4,4 +4,26 @@
 
     //contexto de renderização 2d
     var ctx = cnv.getContext('2d');
+    
+
+    // RECURSOS DO JOGO
+
+
+    // ENTRADAS (Setas do Teclado)
+    var LEFT = 37, RIGHT = 39, ENTER = 13, SPACE = 32;
+
+    // DIREÇÕES (Nave)
+    var mvLeft = mvRight = false;
+
+    //ESTADO DO JOGO (Loading, playing, paused, over)
+    var LOADING = 0, PLAYING = 1, PAUSED = 2, OVER = 3;
+    var gameState = LOADING;
+
+    //LISTENERS (Comandos de Entrada)
+    window.addEventListener('keydown', function(e){
+        var key = e.keyCode;
+        this.alert(key);
+    }, false);
+
+    // FUNÇÕES
 }());
