c9aec696506c35c945deb1fb34f45ccf57ef11c2
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-27 21:31:29 -0300

    Collision.js

---
+++ js/collision.js
@@ -0,0 +1,12 @@
+function collide(s1, s2){
+    var hit = false;
+
+    //Calcular a distancia entre o centro dos sprites
+    var vetX = s1.centerX() - s2.centerX();
+    var vetY = s1.centerY() - s2.centerY();
+
+    //Armazena as somas das metades dos sprites largura e ltura
+    var sumHalfWidth = s1.halfWidth() + s2.halfWidth();
+    var sumHalfHeight = s1.halfHeight() + s2.halfHeight();
+
+}
